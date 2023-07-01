bacf295a314e1b5a025f51d5e74b9ccab99640d1
SajalSatsangi <sajalsatsangi2004@gmail.com>
2023-07-01 19:01:34 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import TextForm from "./components/TextForm";
 function App() {
   return (
     <>
-      <Navbar />
+      <Navbar title="TextUtils" />
       <div className="container my-3">
         <TextForm heading="Enter the text to analyze below" />
       </div>
